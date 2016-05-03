f68927eb939b92e4b500c2544103a55a3e650552
algarvi <algarvi93@gmail.com>
2016-05-03 22:31:44 +0200

    Finalizado

---
+++ workspace/Dibujable/src/casodibujable/VentanaCerrable.java
@@ -0,0 +1,53 @@
+package casodibujable;
+
+import java.awt.*;
+import java.awt.event.*;
+
+class VentanaCerrable extends Frame implements WindowListener {
+	// constructores
+	public VentanaCerrable() {
+		super();
+	}
+
+	public VentanaCerrable(String title) {
+		super(title);
+		setSize(500, 500);
+		addWindowListener(this);
+	}
+
+	// métodos de la interface WindowsListener
+	@Override
+	public void windowActivated(WindowEvent e) {
+		;
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		;
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		;
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		;
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		;
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		;
+	}
+}
